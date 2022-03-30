41f9bebba8e8f4345158137eb78072f09c0270b2
MEET BANTHIA <meetbanthia0304@gmail.com>
2022-03-31 03:43:07 +0530

    added a func till now i am able to find biggest number indices in array also corrected syntax of scanf in main funcion
    made an array inside func of size 'size' which stores
    all indices of biggest number of array if it occurs multiple times in it

--- test.c
+++ test.c
@@ -1,13 +1,41 @@
 // HELLO!!😀
 #include <stdio.h>
 
+void split(int *arr, int size)
+{
+    int big = 0;
+    int store_big[size];
+    int tmp =1;
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > arr[big])
+        {
+            big = i;
+            tmp =1;
+        }
+        else if (arr[i] == arr[big])
+        {
+            store_big[tmp]=i;
+            tmp++;
+        }
+    }
+    //store_big array stores all indices having biggest number of array
+    //indices of stor_big array which stores are from 0 to tmp-1
+    store_big[0]=big;
+
+    for(int i=0;i<tmp-1;i++)
+    {
+        printf("%d ",store_big[i]);
+    }
+}
+
 int main(void)
 // logic
 {
     // input size
     int size;
     printf("Enter the size of array : ");
-    scanf("%d", size);
+    scanf("%d", &size);
     getchar();
 
     int arr[size * 2 - 1];
@@ -16,7 +44,7 @@ int main(void)
     printf("Enter elements of array : ");
     for (int i = 0; i < size; i++)
     {
-        scanf("%d", arr[i]);
+        scanf("%d", &arr[i]);
         getchar();
     }
 
