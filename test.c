44206ae729c47eb161ef9ff61016bd6780441b1c
MEET BANTHIA <meetbanthia0304@gmail.com>
2022-03-31 12:31:24 +0530

    modified func added feature to calculate second highest number in any case
    first i stored diff betwween each element and max alement and
    stored it in atore_less_big array
    now finding minimum positive number in that array
    index corredponding to that array corresponds to second biggest number in array

--- test.c
+++ test.c
@@ -5,28 +5,53 @@ void split(int *arr, int size)
 {
     int big = 0;
     int store_big[size];
-    int tmp =1;
+    int tmp = 1;
     for (int i = 1; i < size; i++)
     {
         if (arr[i] > arr[big])
         {
             big = i;
-            tmp =1;
+            tmp = 1;
         }
         else if (arr[i] == arr[big])
         {
-            store_big[tmp]=i;
+            store_big[tmp] = i;
             tmp++;
         }
     }
-    //store_big array stores all indices having biggest number of array
-    //indices of stor_big array which stores are from 0 to tmp-1
-    store_big[0]=big;
-
-    for(int i=0;i<tmp;i++)
+    // store_big array stores all indices having biggest number of array
+    // indices of stor_big array which stores are from 0 to tmp-1
+    store_big[0] = big;
+    int less;
+    int store_less_big[size];
+    for (int i = 0; i < size; i++)
     {
-        printf("%d ",store_big[i]);
+        store_less_big[i] = arr[big] - arr[i];
     }
+    for (int i = 0; i < size; i++)
+    {
+        if (store_less_big[i] == 0)
+        {
+            continue;
+        }
+        else
+        {
+            less = i;
+            break;
+        }
+    }
+    for(int i=0;i<size;i++)
+    {
+        if (store_less_big[i] == 0)
+        {
+            continue;
+        }
+        else if(store_less_big[i]<store_less_big[less])
+        {
+            less = i;
+        }
+    }
+    printf("%d",arr[less]);
 }
 
 int main(void)
